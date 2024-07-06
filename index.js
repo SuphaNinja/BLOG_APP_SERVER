fb351fa802e02bba8302c84bc5cbc3684238343d
Sid Björk <sidricobjork@gmail.com>
2024-07-06 16:14:12 +0200

    add

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ const fs = require('fs');
 const app = express();
 const prisma = new PrismaClient();
 
-const corsOptions = {
-    origin: 'https://blog-app-client-blue.vercel.app/', 
-    methods: ['GET', 'POST'], 
-    credentials: true,
-};
+
 
 app.use(express.json());
-app.use(cors({ origin: ["https://blog-app-client-blue.vercel.app", "https://blog-app-server-sable.vercel.app"], credentials: true  }));
+app.use(cors({ origin: ["https://blog-app-client-blue.vercel.app", "https://blog-app-server-sable.vercel.app"],}));
+app.set('trust proxy', 1);
 app.use(fileUpload());
 app.use("/images", express.static(__dirname + "/images"));
 
@@ -90,9 +87,6 @@ app.post("/login", async (req,res) => {
 
     if (!loginData.email) { return res.status(404).send({ error: "Email field cannot be left empty!" })};
     if (!loginData.password) { return res.status(404).send({ error: "Password field cannot be left empty!" })};
-    res.setHeader('Access-Control-Allow-Origin', 'https://blog-app-client-blue.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     const user = await prisma.user.findUnique({
         where: { email: loginData.email }
