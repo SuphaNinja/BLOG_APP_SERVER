24de88e7142820754dc0e46e9f477ed5136347b6
Sid Björk <sidricobjork@gmail.com>
2024-07-06 15:58:08 +0200

    add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(fileUpload());
 app.use("/images", express.static(__dirname + "/images"));
 
